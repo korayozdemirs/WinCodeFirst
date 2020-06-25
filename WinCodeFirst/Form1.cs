66ea35fb452e84753db15bf0d5cb7bee9de15617
korayozdemir <koray_ozdemir@msn.com>
2020-06-25 13:53:54 +0300

    Add project files.

---
+++ WinCodeFirst/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinCodeFirst
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SchoolEntities db = new SchoolEntities();
+            var student = new Student { StudentName = "Ahmet taş" };
+            db.Students.Add(student);
+            db.SaveChanges();
+            MessageBox.Show("olduda bitti maşallah");
+        }
+    }
+}
---
+++ WinCodeFirst/SchoolEntities.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinCodeFirst
+{
+    public class SchoolEntities : DbContext
+    {
+        public SchoolEntities() : base("name=SchoolConnection")
+        {
+            Database.SetInitializer<SchoolEntities>(new CreateDatabaseIfNotExists<SchoolEntities>());
+            //Database.SetInitializer<SchoolEntities>(new DropCreateDatabaseIfModelChanges<SchoolEntities>());
+            //Database.SetInitializer<SchoolEntities>(new DropCreateDatabaseAlways<SchoolEntities>());
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+    }
+}
